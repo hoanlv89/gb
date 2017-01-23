3dfbb88bb0e96004826315d5f295f746826fc033
hoanlv89 <hoanlv1989@gmail.com>
2017-01-23 17:41:17 +0700

    banner

---
+++ Assets/Scripts/banner/BannerData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BannerData
+{
+	public int type;
+	public string desc;
+	public string url;
+	public string idPack;
+	public List<BannerItem> arrValue_type20 = new List<BannerItem>();
+	public bool allowClose = true;
+	public Vector2 posClose = new Vector2 (0, 0);
+}
+
---
+++ Assets/Scripts/banner/BannerScene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BannerScene : MonoBehaviour {
+
+	public Button close;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void showBanner(int index){
+	
+	}
+
+	public void exitBanner(){
+		if (gameObject != null && gameObject.activeSelf == true) {
+			gameObject.SetActive(false);
+		}
+	}
+}
